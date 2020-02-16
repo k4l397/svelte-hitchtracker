9206bebd776466823316d8eeea7a077c835c8013
Kyle <kyle.jake.kw@gmail.com>
2020-02-16 23:39:03 +0000

    simplify - it's not as flashy but more simple is almost definitely better than whatever that was before

--- src/LocationForm.svelte
+++ src/LocationForm.svelte
@@ -3,27 +3,37 @@ import * as yup from 'yup';
 import { getPosition } from './utils/helper'
 
 const formValues = {
-  location: '',
-  message: ''
+  location: {value: '', error: ''},
+  message: {value: '', error: ''}
 };
 
 let currentError = -1;
-let errors = null;
+let errors = false;
   
 const schema = yup.object().shape({
-    location: yup.string().required(),
-    message: yup.string().required()
-  });
+    location: yup.object().shape({value: yup.string().required()}),
+    message: yup.object().shape({value: yup.string().required()}),
+});
 
 const handleSubmit = async () => {
   schema.validate(formValues, {abortEarly: false})
     .then((valid) => {
-      errors = null;
+      errors = false;
+      Object.keys(valid).forEach((key) => formValues[key].error = '');
       console.log(valid);
     })
     .catch((err) => {
-      errors = err;
-      console.log(err)
+      errors = true;
+      console.log(err);
+      if (err.inner){
+        err.inner.forEach((innerErr) => {
+          console.log(innerErr.path)
+          formValues[innerErr.path.substr(0, innerErr.path.indexOf('.'))].error = innerErr.message
+        })
+      }
+      else {
+        formValues[err.path.substr(0, innerErr.path.indexOf('.'))].error = err.message
+      }
     })
 };
 
@@ -45,30 +55,27 @@ form {
 }
 
 input {
-  margin: 0.5em 0 0 0
+  margin: 0.5em 0 0 0;
 }
 
 p {
-  margin: 0
+  margin: 0;
+  color: red;
 }
 
 button {
-  margin-top: 0.5em
+  margin-top: 0.5em;
 }
 </style>
 
 <form autocomplete="off" on:submit|preventDefault={handleSubmit}>
-  <input  name="location" type="text" bind:value={formValues.location} placeholder="Location" />
-  {#if errors !== null &&
-    -1 !== (currentError =
-    errors.inner.reduce((errorIndex, err, currentIndex) => {return err.path === "location" ? currentIndex : errorIndex}, -1 ))}
-    <p>{errors.inner[currentError].message}</p>
-  {/if}
-  <input  name="message" type="text" bind:value={formValues.message} placeholder="Message" />
-  {#if errors !== null &&
-    -1 !== (currentError =
-    errors.inner.reduce((errorIndex, err, currentIndex) => {return err.path === "message" ? currentIndex : errorIndex}, -1 ))}
-    <p>{errors.inner[currentError].message}</p>
-  {/if}
+  <input  name="location" type="text" bind:value={formValues.location.value} placeholder="Location" />
+    {#if errors === true && formValues.location.error !== ''}
+      <p>Field is invalid.</p>
+    {/if}
+  <input  name="message" type="text" bind:value={formValues.message.value} placeholder="Message" />
+    {#if errors === true && formValues.message.error !== ''}
+      <p>Field is invalid.</p>
+    {/if}
   <button type="submit">Send Update</button>
 </form>
